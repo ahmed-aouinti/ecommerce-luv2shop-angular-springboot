b03b6d78bc11c31017bd1ca006fa1957291ac63f
Ahmed Aouinti <awintiahmed2017@gmail.com>
2023-10-17 22:16:52 +0200

    add apiUrl and stripe key to product environment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://luv2shop-backend.up.railway.app/api',
+  stripePublishableKey:
+    'pk_test_51NaiSfJ7CqAb5XkluTiXqbDsuJ6IMsWiZgTPCmmKaouA5D3UJGXpXZcFNFM1os4yTZonDreg6w9EUMkk2eagZAE600dy8Thabh',
 };
