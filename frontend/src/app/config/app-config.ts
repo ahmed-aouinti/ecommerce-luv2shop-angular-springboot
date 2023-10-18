0eacd30d7eef572ec9069f973727f5d1158135b4
Ahmed Aouinti <awintiahmed2017@gmail.com>
2023-10-18 23:04:04 +0200

    change okta redirectUri to the https://luv2shop-ecommerce.netlify.app

--- frontend/src/app/config/app-config.ts
+++ frontend/src/app/config/app-config.ts
@@ -2,7 +2,8 @@ export default {
   oidc: {
     clientId: '0oac1xv78jEJHLiLn5d7',
     issuer: 'https://dev-68343085.okta.com/oauth2/default',
-    redirectUri: 'https://localhost:4200/login/callback',
+    // redirectUri: 'https://localhost:4200/login/callback',
+    redirectUri: 'https://luv2shop-ecommerce.netlify.app/login/callback',
     scopes: ['openid', 'profile', 'email'],
   },
 };
