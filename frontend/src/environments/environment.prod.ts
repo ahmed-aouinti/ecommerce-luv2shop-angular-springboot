0970092d97a54fdbdd8d2da0b8559212e54d8b4c
ahmed-bayen <ahmed.okta94@gmail.com>
2023-11-05 22:12:16 +0100

    changed API URL

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://luv2shop-backend.up.railway.app/api',
+  apiUrl: 'https://luv2shop-backend.onrender.com/api',
   stripePublishableKey:
     'pk_test_51NaiSfJ7CqAb5XkluTiXqbDsuJ6IMsWiZgTPCmmKaouA5D3UJGXpXZcFNFM1os4yTZonDreg6w9EUMkk2eagZAE600dy8Thabh',
 };
